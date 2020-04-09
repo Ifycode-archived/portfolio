4abe2c9ff4d02c936848f1a956a02201fe271e92
Obiagba Mary Ifeoma <45185388+Ifycode@users.noreply.github.com>
2020-04-09 22:31:37 +0100

    Homepage design updated

---
+++ js/index.js
@@ -0,0 +1,39 @@
+var body = document.getElementsByTagName('body')[0];
+var h1 = document.getElementsByTagName('h1')[0];
+var nav = document.getElementsByTagName('nav')[0];
+var summary = document.getElementsByTagName('summary')[0];
+
+var intervalCount = 0;
+
+var interval = setInterval(animateHome, 1000);
+
+function animateHome() {
+    intervalCount++;
+    if (intervalCount == 1) {
+        body.style.gridTemplateRows = '1fr 110px';
+        summary.style.height = 'calc(100vh - 110px)';
+        nav.style.opacity = '1';
+        clearInterval(interval);
+        window.onresize = function() {
+            summary.style.transition = 'none';
+        }
+        
+    }
+}
+
+window.onorientationchange = function() {
+    summary.style.transition = 'none';
+}
+
+
+var menuIcon = document.getElementsByClassName('menu-icon')[0];
+
+menuIcon.onclick = function() {
+    alert('Not working yet. Site still under construction');
+}
+
+var searchContainer = document.getElementsByClassName('search-input-container')[0];
+
+searchContainer.onclick = function() {
+    alert('Not working yet. Site still under construction');
+}
