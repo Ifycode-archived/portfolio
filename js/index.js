6f83334ae4f95ef33f2ff7af10b43933bb254af4
Obiagba Mary Ifeoma <45185388+Ifycode@users.noreply.github.com>
2020-04-09 23:05:17 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,10 +10,8 @@ var interval = setInterval(animateHome, 1000);
 function animateHome() {
     intervalCount++;
     if (intervalCount == 1) {
-        /*
-        body.style.gridTemplateRows = '1fr 110px';
-        */
-        summary.style.height = 'calc(100vh - 110px)';
+        body.style.gridTemplateRows = '1fr 210px';
+        summary.style.height = 'calc(100vh - 210px)';
         nav.style.opacity = '1';
         clearInterval(interval);
         window.onresize = function() {
